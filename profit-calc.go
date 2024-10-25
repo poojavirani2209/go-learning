73e8e253b1c49f0410bfee63383405f31d1691f8
pooja virani <pooja1522@gmail.com>
2024-10-25 16:41:43 +0530

    Use functions, to make code readable and organized with reusability

--- profit-calc.go
+++ profit-calc.go
@@ -3,18 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	fmt.Print("Tell me your revenue:")
-	fmt.Scan(&revenue)
-	var expenses float64
-	fmt.Print("Tell me your expenses:")
-	fmt.Scan(&expenses)
-	var tax float64
-	fmt.Print("Tell me your tax rate:")
-	fmt.Scan(&tax)
+	revenue := getInput("Tell me your revenue")
+	expenses := getInput("Tell me your expenses")
+	tax := getInput("Tell me your tax rate")
 
-	earnBeforeTax := revenue - expenses
-	profit := earnBeforeTax * (1 - tax/100)
+	earnBeforeTax, profit := calculateProfit(revenue, expenses, tax)
 
 	fmt.Println("Your earnings before tax:")
 	fmt.Println(earnBeforeTax)
@@ -24,4 +17,17 @@ func main() {
 
 	fmt.Println("Ratio")
 	fmt.Println(earnBeforeTax / profit)
+}
+
+func calculateProfit(revenue float64, expenses float64, tax float64) (float64, float64) {
+	earnBeforeTax := revenue - expenses
+	profit := earnBeforeTax * (1 - tax/100)
+	return earnBeforeTax, profit
+}
+
+func getInput(inputExpected string) float64 {
+	var inputValueProvided float64
+	fmt.Println(inputExpected)
+	fmt.Scan(&inputValueProvided)
+	return inputValueProvided
 }
